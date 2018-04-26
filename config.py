9863c73a88614f1dd6de7c96b07ba5c2ee3e301d
Kamil <even23@wp.pl>
2018-04-26 16:08:19 +0200

    lbp_parts

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ BOUNDING_BOXES_PATH = os.path.join(RESOURCES_PATH, 'bounding_boxes').replace('\\
 IMAGE_FORMAT = 'jpg'
 IMAGE_SIZE = 224
 IMAGE_BORDER_STYLE = cv2.BORDER_DEFAULT  # reflect image on borders
+PART_SIZE = 56
 
 LBP_RADIUS = 1
 LBP_POINTS_NUMBER = LBP_RADIUS * 8
--- preprocessing/lbp.py
+++ preprocessing/lbp.py
@@ -1,4 +1,5 @@
 import cv2
+import config
 
 import numpy as np
 from skimage.feature import local_binary_pattern
@@ -12,9 +13,14 @@ class LocalBinaryPattern:
 
     def describe(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        histogram = list()
 
-        lbp = local_binary_pattern(image, self.n_points, self.radius, self.method)
-        n_bins = int(lbp.max() + 1)
-        hist, _ = np.histogram(lbp.ravel(), bins=n_bins, range=(0, n_bins), density=True)
+        for r in range(0, image.shape[0] + 1 - config.PART_SIZE, config.PART_SIZE):
+            for c in range(0, image.shape[1] + 1 - config.PART_SIZE, config.PART_SIZE):
+                window = image[r:r + config.PART_SIZE, c:c + config.PART_SIZE]
+                lbp = local_binary_pattern(window, self.n_points, self.radius, self.method)
+                n_bins = int(lbp.max() + 1)
+                hist, _ = np.histogram(lbp.ravel(), bins=n_bins, range=(0, n_bins), density=True)
+                histogram.extend(hist)
 
-        return hist
+        return np.array(histogram)
