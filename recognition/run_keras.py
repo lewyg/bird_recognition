5ecfd35b09e38fc052d0e5301259994cd3884fda
Jan Antoniak <jan.antoniak@semantive.com>
2018-04-28 07:11:50 +0200

    Added plot of acc/epoch for top-1 and top 5 error rate.

--- recognition/run_keras.py
+++ recognition/run_keras.py
@@ -8,7 +8,7 @@ import keras.metrics as metrics
 from matplotlib import pyplot
 
 
-def rmse(y_true, y_pred):
+def t5er(y_true, y_pred):
     return metrics.top_k_categorical_accuracy(y_true, y_pred, 5)
 
 
@@ -29,18 +29,18 @@ def main(layers=4):
 
     clf.add(Dense(output_layer_size, activation='softmax'))
 
-    clf.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+    clf.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc', t5er])
 
     y_train = keras.utils.to_categorical(np.array(y_train), num_classes=output_layer_size)
     y_test = keras.utils.to_categorical(np.array(y_test), num_classes=output_layer_size)
 
-    history = clf.fit(np.array(X_train), y_train, epochs=50, batch_size=32, verbose=2)
+    history = clf.fit(np.array(X_train), y_train, epochs=500, batch_size=32, verbose=2)
 
     print(clf.evaluate(np.array(X_test), y_test, batch_size=32))
+
     pyplot.plot(history.history['acc'])
+    pyplot.plot(history.history['t5er'])
     pyplot.show()
-
-
 
 
 if __name__ == '__main__':
